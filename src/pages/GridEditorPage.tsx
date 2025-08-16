122ca5d481023e124d5214d157e4d00ea82e7977
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-08-16 16:27:04 +0100

    read grid shape as a test

--- src/pages/GridEditorPage.tsx
+++ src/pages/GridEditorPage.tsx
@@ -1,3 +1,12 @@
+import {useGridShape} from "../util/grid";
+
 export const GridEditorPage = () => {
-    return <h1 className="text-2xl font-bold">Edit grid</h1>;
+    const [shape] = useGridShape();
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold">Edit grid</h1>
+            <p>{shape.cols}, {shape.rows}</p>
+        </>
+    );
 }
